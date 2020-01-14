eac3136591ee467938631d5d92a786c78bcf82ce
Karen Oliveira <karen.oliveira0205@gmail.com>
2020-01-14 12:17:00 -0300

    Booleans and Comparisons Finalized

---
+++ chapter2/booleans-comparison.js
@@ -0,0 +1,21 @@
+console.log("Booleans and Comparison");
+
+console.log(true,false);
+
+let email = 'karen.oliveira0205@gmail.com';
+let resultEmailInclude = email.includes('@');
+let names = ['mario','luigi','toad'];
+let resultNamesInclude = names.includes("luigi");
+
+console.log('ResultEmailInclude:',resultEmailInclude);
+console.log('ResultNamesInclude:',resultNamesInclude);
+
+let age = 25;
+console.log('Comparison 25:',age == 25);
+console.log('Comparison 30:',age == 30);
+console.log('Comparison not30:',age != 30);
+console.log('Comparison higher30:',age > 30);
+
+let name = 'shaun';
+console.log('name > crystal',name > 'crystal');
+console.log('name < crystal',name < 'crystal');
