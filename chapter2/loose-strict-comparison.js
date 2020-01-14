268e4db26b9c1d40453072e8710a93ea4f2a6b89
Karen Oliveira <karen.oliveira0205@gmail.com>
2020-01-14 14:11:45 -0300

    Loose and Strict Comparison

---
+++ chapter2/loose-strict-comparison.js
@@ -0,0 +1,10 @@
+console.log('Loose and Strict Comparisons');
+let age = 25;
+
+// == convert and evaluate 
+console.log(age == 25);
+console.log(age == '25');
+
+//=== compare types too
+console.log(age === 25);
+console.log(age === '25');
