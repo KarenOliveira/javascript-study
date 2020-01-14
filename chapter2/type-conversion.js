ae07a04527e7b06994b2adceb3118379e22e3933
Karen Oliveira <karen.oliveira0205@gmail.com>
2020-01-14 14:23:26 -0300

    Chapter 2 - Finalized

---
+++ chapter2/type-conversion.js
@@ -0,0 +1,12 @@
+console.log('Type Conversion');
+let score = '100';
+
+score = Number(score);
+console.log(score + 1);
+console.log(typeof score);
+
+let result = Number('hello');
+console.log(result);
+
+let str = String(score);
+console.log(str);
