93b52be2c2960ef084240d8e94632340fff9dd74
Karen Oliveira <karen.oliveira0205@gmail.com>
2020-01-17 17:22:00 -0300

    Starting Block Scope

---
+++ chapter3/switch.js
@@ -0,0 +1,20 @@
+console.log('Switch');
+
+const grade = 'D';
+
+switch (grade) {
+    case 'A':
+        console.log('You got an A!');
+        break;
+    case 'B':
+        console.log('You got an B!');
+        break;
+    case 'C':
+        console.log('You got an C!');
+        break;
+    case 'D':
+        console.log('You got an D!');
+        break;
+    default:
+        console.log('Not a valid grade');
+}
