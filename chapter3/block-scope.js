d3fd95ff9e5586119619b5cf7a7d2b8764bc36d0
Karen Oliveira <karen.oliveira0205@gmail.com>
2020-01-20 13:49:30 -0300

    Chapter 3 - Finalized

---
+++ chapter3/block-scope.js
@@ -0,0 +1,15 @@
+console.log('Block Scope');
+
+let age = 30;
+
+if(true){
+    let age = 40;
+    let name = 'shaun';
+    console.log('Inside 1st code block: ',age,name);
+    if(true){
+        let age = 50;
+        console.log('inside 2nd code block: ',age);
+    }
+}
+
+console.log('outside code block ',age,name);
