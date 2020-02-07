9880063e833f91570bd7e42203975c3683682111
Karen Oliveira <karen.oliveira0205@gmail.com>
2020-02-07 12:04:34 -0300

    Lecture 52 - Finalized

---
+++ chapter6/parents-children-siblings.js
@@ -0,0 +1,9 @@
+console.log('Parents, Children and Siblings');
+
+const article = document.querySelector('article');
+
+//console.log(article.children);
+
+const title = document.querySelector('h2');
+console.log(title.parentElement);
+console.log(title.parentElement.parentElement);
